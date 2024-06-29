a0301b68977da1df5d394ec627fe1fede9787282
EDIH <illia.averyanov@gmail.com>
2024-06-29 15:37:00 +0300

    add routes, buttons, api

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-import './bootstrap';
+import "./bootstrap";
+import router from "./router";
+import { createApp } from "vue";
+import axios from "axios";
+import { ref } from "vue";
+
+import App from "./App.vue";
+
+createApp(App)
+    .use(router)
+    .mount("#app");
+
+
+
+const response = ref();
+
+const getValue = async () => {
+    try {
+        response.value = await axios.get("/api/test");
+    } catch (error) {
+        // Do something with the error
+        console.log(error);
+    }
+};
