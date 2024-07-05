569b91288469edf4c33065c665b4e77badbb0750
EDIH <illia.averyanov@gmail.com>
2024-07-05 18:02:53 +0300

    add element ui form start

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,9 +2,10 @@
     <h1>
         FILTER
     </h1>
-    <router-view v-slot="{ Component, route }">
-        <div :key="route.name">
-            <Component :is="Component" />
-        </div>
-    </router-view>
+<!--    <router-view v-slot="{ Component, route }">-->
+<!--        <div :key="route.name">-->
+<!--            <Component :is="Component" />-->
+<!--        </div>-->
+<!--    </router-view>-->
+    <v-form-designer></v-form-designer>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,21 @@ import router from "./router";
 import { createApp } from "vue";
 import axios from "axios";
 import { ref } from "vue";
+import ElementPlus from "element-plus";
+import VForm3 from "vform3-builds";
+
+import 'element-plus/dist/index.css'  //引入element-plus样式
+import 'vform3-builds/dist/designer.style.css'  //引入VForm3样式
 
 import App from "./App.vue";
 
 createApp(App)
     .use(router)
+    .use(ElementPlus)
+    .use(VForm3)
     .mount("#app");
 
-
+window.axios = axios
 
 const response = ref();
 
